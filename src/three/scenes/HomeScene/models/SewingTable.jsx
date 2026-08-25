760477a75fb803012ded6ece4b3419eac6bea3ed
Amma-Zhi <91179029+Amma-Zhi@users.noreply.github.com>
2026-08-25 13:55:19 +0800

    refactor: widen sewing table to match reference proportions

--- src/three/scenes/HomeScene/models/SewingTable.jsx
+++ src/three/scenes/HomeScene/models/SewingTable.jsx
@@ -26,7 +26,7 @@ export default function SewingTable() {
       <RoundedBox
         castShadow
         receiveShadow
-        args={[5.05, 0.28, 1.65]}
+        args={[5.3, 0.28, 1.65]}
         radius={0.16}
         smoothness={7}
         position={[0, 2.18, 0]}
@@ -34,14 +34,14 @@ export default function SewingTable() {
         <meshStandardMaterial {...clayMaterialProps.primary} />
       </RoundedBox>
 
-      <RoundedBox castShadow args={[4.45, 0.34, 1.18]} radius={0.1} smoothness={5} position={[0, 1.9, -0.05]}>
+      <RoundedBox castShadow args={[4.7, 0.34, 1.18]} radius={0.1} smoothness={5} position={[0, 1.9, -0.05]}>
         <meshStandardMaterial {...clayMaterialProps.secondary} />
       </RoundedBox>
 
-      <TableLeg position={[-2.08, 1.02, 0.54]} rotationZ={0.055} />
-      <TableLeg position={[2.08, 1.02, 0.54]} rotationZ={-0.055} />
-      <TableLeg position={[-2.08, 1.02, -0.54]} rotationZ={0.035} />
-      <TableLeg position={[2.08, 1.02, -0.54]} rotationZ={-0.035} />
+      <TableLeg position={[-2.18, 1.02, 0.54]} rotationZ={0.055} />
+      <TableLeg position={[2.18, 1.02, 0.54]} rotationZ={-0.055} />
+      <TableLeg position={[-2.18, 1.02, -0.54]} rotationZ={0.035} />
+      <TableLeg position={[2.18, 1.02, -0.54]} rotationZ={-0.035} />
     </group>
   )
 }
